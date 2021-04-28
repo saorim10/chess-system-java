7a441a244dfb9fdb85fddd9c1cce1130d7f995c8
Marcelo Rocha Saorim <marcelo.saorim@gmail.com>
2021-04-28 00:28:05 -0300

    Chess layer and printing board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -27,4 +27,11 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece pience(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 }
